984aba6d0f5c69fc5ead149b8d90acaa60bbfa5b
Joshua Koike <jkoike2013@gmail.com>
2016-11-04 09:58:50 -0700

    Completed Channel endpoints

--- src/Network/Discord/Rest/Channel.hs
+++ src/Network/Discord/Rest/Channel.hs
@@ -21,7 +21,7 @@ module Network.Discord.Rest.Channel
 
     data ChannelRequest a where
       GetChannel              :: Snowflake -> ChannelRequest Channel
-      ModifyChannel           :: ToJSON a => Snowflake -> a -> ChannelRequest Channel
+      ModifyChannel           :: ToJSON a  => Snowflake -> a -> ChannelRequest Channel
       DeleteChannel           :: Snowflake -> ChannelRequest Channel
       GetChannelMessages      :: Snowflake -> [(Text, Text)] -> ChannelRequest [Message]
       GetChannelMessage       :: Snowflake -> Snowflake -> ChannelRequest Message
@@ -30,9 +30,9 @@ module Network.Discord.Rest.Channel
       EditMessage             :: Message   -> Text -> ChannelRequest Message
       DeleteMessage           :: Message   -> ChannelRequest ()
       BulkDeleteMessage       :: Snowflake -> [Message] -> ChannelRequest ()
-      EditChannelPermissions  :: Snowflake -> Snowflake -> ChannelRequest ()
+      EditChannelPermissions  :: ToJSON a  => Snowflake -> Snowflake -> a -> ChannelRequest ()
       GetChannelInvites       :: Snowflake -> ChannelRequest Object
-      CreateChannelInvite     :: Snowflake -> ChannelRequest Object
+      CreateChannelInvite     :: ToJSON a  => Snowflake -> a -> ChannelRequest Object
       DeleteChannelPermission :: Snowflake -> Snowflake -> ChannelRequest ()
       TriggerTypingIndicator  :: Snowflake -> ChannelRequest ()
       GetPinnedMessages       :: Snowflake -> ChannelRequest [Message]
@@ -52,9 +52,9 @@ module Network.Discord.Rest.Channel
       hashWithSalt s (DeleteMessage (Message _ chan _ _ _ _ _ _ _ _ _ _ _ _)) =
         hashWithSalt s ("get_msg"::Text, chan)
       hashWithSalt s (BulkDeleteMessage chan _) = hashWithSalt s ("del_msgs"::Text, chan)
-      hashWithSalt s (EditChannelPermissions chan _) = hashWithSalt s ("perms"::Text, chan)
+      hashWithSalt s (EditChannelPermissions chan _ _) = hashWithSalt s ("perms"::Text, chan)
       hashWithSalt s (GetChannelInvites chan) = hashWithSalt s ("invites"::Text, chan)
-      hashWithSalt s (CreateChannelInvite chan) = hashWithSalt s ("invites"::Text, chan)
+      hashWithSalt s (CreateChannelInvite chan _) = hashWithSalt s ("invites"::Text, chan)
       hashWithSalt s (DeleteChannelPermission chan _) = hashWithSalt s ("perms"::Text, chan)
       hashWithSalt s (TriggerTypingIndicator chan)  = hashWithSalt s ("tti"::Text, chan)
       hashWithSalt s (GetPinnedMessages chan) = hashWithSalt s ("pins"::Text, chan)
@@ -88,10 +88,74 @@ module Network.Discord.Rest.Channel
       req  <- baseRequest
       (resp, rlRem, rlNext) <- lift $ do
         resp <- case request of
+          GetChannel chan -> getWith req
+            (baseURL++"/channels/"++chan)
+
+          ModifyChannel chan patch -> customPayloadMethodWith "PATCH" req
+            (baseURL++"/channels/"++chan)
+            (toJSON patch)
+
+          DeleteChannel chan -> deleteWith req
+            (baseURL++"/channels/"++chan)
+
+          GetChannelMessages chan patch -> getWith
+            (Prelude.foldr (\(k, v) -> param k .~ [v]) req patch)
+            (baseURL++"/channels/"++chan++"/messages")
+
+          GetChannelMessage chan msg -> getWith req
+            (baseURL++"/channels/"++chan++"/messages/"++msg)
+
           CreateMessage chan msg -> postWith req
-            (baseURL ++ "/channels/" ++ chan ++ "/messages")
-            (toJSON $ object [("content", toJSON msg)])
-          _ -> error "Unsupported call"
+            (baseURL++"/channels/"++chan++"/messages")
+            (object [("content", toJSON msg)])
+
+          UploadFile chan msg file -> postWith
+            (req & header "Content-Type" .~ ["multipart/form-data"])
+            (baseURL++"/channels/"++chan++"/messages")
+            ["content" := msg, "file" := file]
+
+          EditMessage (Message msg chan _ _ _ _ _ _ _ _ _ _ _ _) new ->
+            customPayloadMethodWith "PATCH" req
+              (baseURL++"/channels/"++chan++"/messages/"++msg)
+              (object [("content", toJSON new)])
+
+          DeleteMessage (Message msg chan _ _ _ _ _ _ _ _ _ _ _ _) ->
+            deleteWith req
+              (baseURL++"/channels/"++chan++"/messages/"++msg)
+
+          BulkDeleteMessage chan msgs -> postWith req
+            (baseURL++"/channels/"++chan++"/messages/bulk-delete")
+            (object
+              [("messages", toJSON
+                $ Prelude.map (\(Message msg _ _ _ _ _ _ _ _ _ _ _ _ _) -> msg) msgs)])
+
+          EditChannelPermissions chan perm patch -> putWith req
+            (baseURL++"/channels/"++chan++"/permissions/"++perm)
+            (toJSON patch)
+
+          GetChannelInvites chan -> getWith req
+            (baseURL++"/channels/"++chan++"/invites")
+
+          CreateChannelInvite chan patch -> postWith req
+            (baseURL++"/channels/"++chan++"/invites")
+            (toJSON patch)
+
+          DeleteChannelPermission chan perm -> deleteWith req
+            (baseURL++"/channels/"++chan++"/permissions/"++perm)
+
+          TriggerTypingIndicator chan -> postWith req
+            (baseURL++"/channels/"++chan++"/typing")
+            (toJSON ([]::[Int]))
+
+          GetPinnedMessages chan -> getWith req
+            (baseURL++"/channels/"++chan++"/pins")
+
+          AddPinnedMessage chan msg -> putWith req
+            (baseURL++"/channels/"++chan++"/pins/"++msg)
+            (toJSON ([]::[Int]))
+
+          DeletePinnedMessage chan msg -> deleteWith req
+            (baseURL++"/channels/"++chan++"/pins/"++msg)
         return (justRight . eitherDecode $ resp ^. responseBody
           , justRight . eitherDecodeStrict $ resp ^. responseHeader "X-RateLimit-Remaining"::Int
           , justRight . eitherDecodeStrict $ resp ^. responseHeader "X-RateLimit-Reset"::Int)
