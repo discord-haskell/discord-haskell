1a679c8dff43ae36eceea45acad40b7b90a1d092
Joshua Koike <jkoike2013@gmail.com>
2016-11-07 10:09:52 -0700

    Added logbot and pingpong examples

---
+++ examples/logbot.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Monad.State
+import Data.Text
+
+import Pipes ((~>))
+import Pipes.Core hiding (Client)
+import System.Remote.Monitoring
+
+import Network.Discord.Types
+import Network.Discord.Rest
+import Network.Discord.Gateway
+
+data LogClient = LogClient
+instance Client LogClient where
+  getAuth _ = "MjMyMjg0Mzg5NDIwMTcxMjY0.Cv6rgw.uE6wqrKoLWNS3eG1Jgb4S_ZsNQw"
+
+main :: IO ()
+main = do
+  forkServer "localhost" 8080
+  gateway <- getGateway
+  runWebsocket gateway LogClient $ do
+    DiscordState {getWebSocket=ws} <- get
+    (eventCore ~> \event -> case event of
+      Ready (Init v u _ _ _) -> liftIO . putStrLn $ "Connected to gateway v"++show v
+        ++ " as user " ++ show u
+      MessageCreate msg@(Message _ _ (User _ _ _ _ bot _ _ _) _ _ _ _ _ _ _ _ _ _ _) ->
+        unless bot $
+          void (restServer +>> fetch $ CreateMessage "188134500411244545" . pack $ show msg)
+      ev -> void (restServer +>> fetch $ CreateMessage "188134500411244545" . pack $ show ev)
+      ) ws
---
+++ examples/pingpong.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Control.Monad.State
+import Data.Text
+
+import Pipes ((~>))
+import Pipes.Core hiding (Client)
+import System.Remote.Monitoring
+
+import Network.Discord.Types
+import Network.Discord.Rest
+import Network.Discord.Gateway
+
+data PingPongClient = PingPongClient
+instance Client PingPongClient where
+  getAuth _ = "MjMyMjg0Mzg5NDIwMTcxMjY0.Cv6rgw.uE6wqrKoLWNS3eG1Jgb4S_ZsNQw"
+
+main :: IO ()
+main = do
+  forkServer "localhost" 8080
+  gateway <- getGateway
+  runWebsocket gateway PingPongClient $ do
+    DiscordState {getWebSocket=ws} <- get
+    (eventCore ~> \event -> case event of
+      Ready (Init v u _ _ _) -> liftIO . putStrLn $ "Connected to gateway v"++show v
+        ++ " as user " ++ show u
+      MessageCreate (Message _ chan (User _ _ _ _ bot _ _ _) cont _ _ _ _ _ _ _ _ _ _) ->
+        when ("Ping" `isPrefixOf` cont && not bot) $
+          void (restServer +>> (request . Fetch $ CreateMessage chan "Pong!"))
+      ev -> liftIO $ print ev
+      ) ws
