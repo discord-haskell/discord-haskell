717bbae129bc52518b25e049a30135570e3171a5
Joshua Koike <jkoike2013@gmail.com>
2016-11-07 13:47:45 -0700

    Added core re-exports

---
+++ src/Network/Discord.hs
@@ -0,0 +1,12 @@
+module Network.Discord
+  (
+    module Network.Discord
+  , module Discord
+  , module Re
+  ) where
+    import Network.Discord.Rest as Discord
+    import Network.Discord.Types as Discord
+    import Network.Discord.Gateway as Discord
+    import Pipes as Re ((~>), (>->), yield, await)
+    import Pipes.Core as Re ((+>>))
+    import Control.Monad.State as Re (get, put, liftIO, void, when, unless)
