da82af8046481da2041312cf9f93b360fe0a7feb
Joshua Koike <jkoike2013@gmail.com>
2016-11-08 11:54:29 -0700

    Updated pingpong with new re-exports

--- examples/pingpong.hs
+++ examples/pingpong.hs
@@ -1,14 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Control.Monad.State
 import Data.Text
 
-import Pipes ((~>))
-import Pipes.Core hiding (Client)
-import System.Remote.Monitoring
-
-import Network.Discord.Types
-import Network.Discord.Rest
-import Network.Discord.Gateway
+import Network.Discord
 
 data PingPongClient = PingPongClient
 instance Client PingPongClient where
@@ -16,7 +9,6 @@ instance Client PingPongClient where
 
 main :: IO ()
 main = do
-  forkServer "localhost" 8080
   gateway <- getGateway
   runWebsocket gateway PingPongClient $ do
     DiscordState {getWebSocket=ws} <- get
@@ -25,6 +17,6 @@ main = do
         ++ " as user " ++ show u
       MessageCreate (Message _ chan (User _ _ _ _ bot _ _ _) cont _ _ _ _ _ _ _ _ _ _) ->
         when ("Ping" `isPrefixOf` cont && not bot) $
-          void (restServer +>> (request . Fetch $ CreateMessage chan "Pong!"))
-      ev -> liftIO $ print ev
+          void $ restServer +>> fetch (CreateMessage chan "Pong!")
+      _ -> return ()
       ) ws
