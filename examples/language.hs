8127a50ab8fa0735d5a9f6cdf1a0b25f6c68a4c1
Joshua Koike <jkoike@gmail.com>
2017-01-17 12:40:35 -0700

    Changes reflecting type refinements

--- examples/language.hs
+++ examples/language.hs
@@ -8,7 +8,7 @@ import Control.Monad.IO.Class
 main = runBot "TOKEN" $ do
   with ReadyEvent $ \_ -> do
     liftIO $ putStr "Hello, World!"
-  with MessageCreateEvent $ \(MessageCreate (msg@Message{messageAuthor=User{userIsBot=bot}})) -> do
+  with MessageCreateEvent $ \msg@Message{messageAuthor=User{userIsBot=bot}} -> do
     liftIO $ print msg
     unless bot $
       fetch' $ CreateMessage "188134500411244545" . pack $ show msg
