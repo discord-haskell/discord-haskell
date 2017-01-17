57f31d71d482fb6741d42539f22c3120d70e22cb
Joshua Koike <jkoike2013@gmail.com>
2017-01-16 19:19:03 -0700

    Add language example

--- examples/language.hs
+++ examples/language.hs
@@ -1,14 +1,14 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+import Network.Discord
+import Pipes
+import Data.Text
 import Language.Discord
+import Control.Monad.IO.Class
 
-main = runBot "TOKEN" $
-  on MessageCreate $
-    doCommand "`" $
-          command "userInfo" $ do
-            user <- author
-            send $ show user
-      <|> command "ping" $ send "Pong"
-      <|> command "channelInfo" $ do
-            chanId <- channel
-            chanInfo <- fetch (GetChannel chanId)
-            send $ show chanInfo
+main = runBot "TOKEN" $ do
+  with ReadyEvent $ \_ -> do
+    liftIO $ putStr "Hello, World!"
+  with MessageCreateEvent $ \(MessageCreate (msg@Message{messageAuthor=User{userIsBot=bot}})) -> do
+    liftIO $ print msg
+    unless bot $
+      fetch' $ CreateMessage "188134500411244545" . pack $ show msg
