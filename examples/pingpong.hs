0750fb46c327e08258435b3f1cc6b3f457df200e
Joshua Koike <jkoike@gmail.com>
2017-01-17 15:17:52 -0700

    Updated pingpong example

--- examples/pingpong.hs
+++ examples/pingpong.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
 import Data.Text
 import Pipes
 
@@ -10,9 +10,9 @@ reply Message{messageChannel=chan} cont = fetch' $ CreateMessage chan cont
 
 main :: IO ()
 main = runBot "TOKEN" $ do
-  with ReadyEvent $ \_ ->
-    liftIO $ putStrLn "Online"
+  with ReadyEvent $ \(Init v u _ _ _) ->
+    liftIO . putStrLn $ "Connected to gateway v" ++ show v ++ " as user " ++ show u
 
-  with MessageCreateEvent $ \(MessageCreate msg) -> do
-    when (messageContent msg == "Ping!") $
+  with MessageCreateEvent $ \msg@Message{..} -> do
+    when ("Ping" `isPrefixOf` messageContent && (not . userIsBot $ messageAuthor)) $
       reply msg "Pong!"
