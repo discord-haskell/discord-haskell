01fbf65cec0595c5fa4380927ef5b0b517e51fd8
Joshua Koike <jkoike2013@gmail.com>
2017-02-08 20:21:57 -0700

    Add support for different auth tokens

--- src/Network/Discord/Types/Prelude.hs
+++ src/Network/Discord/Types/Prelude.hs
@@ -5,13 +5,27 @@ module Network.Discord.Types.Prelude where
   import Data.Time.Clock.POSIX
   import Data.Bits
 
-  type Auth = String
+  data Auth = Bot    String
+            | Client String
+            | Bearer String
+
+  instance Show Auth where
+    show (Bot    token) = "Bot "    ++ token
+    show (Client token) = token
+    show (Bearer token) = "Bearer " ++ token
+
+  authToken :: Auth -> String
+  authToken (Bot    token) = token
+  authToken (Client token) = token
+  authToken (Bearer token) = token
+
   -- |A unique integer identifier. Can be used to calculate the creation date of an entity.
   type Snowflake = String
 
   -- |Gets a creation date from a snowflake.
   creationDate :: Snowflake -> UTCTime
-  creationDate x = posixSecondsToUTCTime $ realToFrac(1420070400 + (((read x :: Int) `shiftR` 22) `quot` 1000))
+  creationDate x = posixSecondsToUTCTime . realToFrac
+    $ 1420070400 + quot (shiftR (read x :: Int) 22) 1000
 
   epochTime :: UTCTime
   epochTime = posixSecondsToUTCTime $ realToFrac(0 :: Int)
