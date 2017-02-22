4b9283c8484fcfe9ad31970fad0d74c0fb9ff8dc
Stan Serebryakov <sserebryakov@luxoft.com>
2017-02-22 14:34:59 +0300

    Keep only REST

--- src/Network/Discord.hs
+++ src/Network/Discord.hs
@@ -2,12 +2,10 @@ module Network.Discord
   (
     module Network.Discord.Rest
   , module Network.Discord.Types
-  , module Network.Discord.Gateway
   , module Re
   ) where
     import Network.Discord.Rest
     import Network.Discord.Types
-    import Network.Discord.Gateway
     import Pipes as Re ((~>), (>->), yield, await)
     import Pipes.Core as Re ((+>>))
     import Control.Monad.State as Re (get, put, liftIO, void, when, unless)
--- src/Network/Discord/Types.hs
+++ src/Network/Discord/Types.hs
@@ -4,23 +4,20 @@ module Network.Discord.Types
   ( module Network.Discord.Types
   , module Network.Discord.Types.Channel
   , module Network.Discord.Types.Guild
-  , module Network.Discord.Types.Events
-  , module Network.Discord.Types.Gateway
   , module Network.Discord.Types.Prelude
   ) where
     import Control.Monad.State (StateT)
 
     import Control.Concurrent.STM
-    import Network.WebSockets (Connection)
 
     import Network.Discord.Types.Channel
     import Network.Discord.Types.Guild
-    import Network.Discord.Types.Events
-    import Network.Discord.Types.Gateway
     import Network.Discord.Types.Prelude
 
     class Client c where
       getAuth :: c -> Auth
+
+    data Connection
 
     -- | A state of discord connection.
     data StateEnum = Create | Start | Running | InvalidReconnect | InvalidDead
