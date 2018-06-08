16d44b81baab5923f1735e5caca097f07a10e7f5
Karl <37496339+aquarial@users.noreply.github.com>
2018-06-08 18:30:23 -0500

    Fix Bad file descriptor error, remove debug prints
    Error looks like
    <interactive>: Network.Socket.sendBuf: invalid argument (Bad file descriptor)
    <interactive>: ConnectionClosed
    
    Happens because runSecureClient has to have all the actions happen
    in the same thread (no forkIO)

--- discord-gateway/src/Network/Discord/Gateway.hs
+++ discord-gateway/src/Network/Discord/Gateway.hs
@@ -43,19 +43,17 @@ data GatewayState
 
 newSocket :: DiscordAuth -> Chan Event -> IO ()
 newSocket (DiscordAuth auth _) events = do
-  runSecureClient "gateway.discord.gg" 443 "/?v=6" $ \connection -> do
+  forkIO $
+    runSecureClient "gateway.discord.gg" 443 "/?v=6&encoding=json" $ \connection -> do
       Hello interval <- step connection
-      print interval
       sequenceKey <- newIORef (-1)
       forkIO $ heartbeat connection interval sequenceKey
-      forkIO $ eventStream connection auth sequenceKey events
+      eventStream connection auth sequenceKey events
   return ()
 
 step :: Connection -> IO Payload
 step connection = do
-  putStrLn "Waiting for data"
   msg' <- receiveData connection
-  putStrLn "Got data"
   case eitherDecode msg' of
     Right msg -> return msg
     Left  err -> do putStrLn ("Discord-hs.Gateway.Parse" <> err)
@@ -66,19 +64,14 @@ heartbeat :: Connection -> Int -> IORef Integer -> IO ()
 heartbeat conn interval sequenceKey = forever $ do
   num <- readIORef sequenceKey
   send conn (Heartbeat num)
-  print "heart"
   threadDelay (interval * 1000)
-  print "beat"
 
 setSequence :: IORef Integer -> Integer -> IO ()
 setSequence = writeIORef
 
 send :: Connection -> Payload -> IO ()
-send conn payload = do
-  print ("Sending: " <> show payload)
-  print ("sending: " <> (show (encode payload)))
+send conn payload =
   sendTextData conn (encode payload)
-  print "sent"
 
 eventStream :: Connection -> Auth -> IORef Integer -> Chan Event -> IO ()
 eventStream conn auth sequenceKey eventChan = loop Start
@@ -95,9 +88,7 @@ eventStream conn auth sequenceKey eventChan = loop Start
         case parseDispatch payload of
           Left reason -> print ("Discord-hs.Gateway.Dispatch - " <> reason)
           Right event -> do
-            print event
             writeChan eventChan event
-        putStrLn "Stepping app"
         loop Running
       Heartbeat sq -> do
         setSequence sequenceKey sq
