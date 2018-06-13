dd4bd50f07fbf9ad9c0ddfe56a0b60be793d7851
Karl <37496339+aquarial@users.noreply.github.com>
2018-06-13 13:25:33 -0500

    Add syncronized stdout loggint to gateway

--- discord-gateway/src/Network/Discord/Gateway.hs
+++ discord-gateway/src/Network/Discord/Gateway.hs
@@ -5,7 +5,7 @@
 --   people will need, and you should use Language.Discord.
 module Network.Discord.Gateway where
 
-import Control.Monad (void)
+import Control.Monad (void, forever)
 import Control.Concurrent.Chan
 import Control.Concurrent (threadDelay, forkIO)
 import Control.Monad (forever)
@@ -26,28 +26,40 @@ data GatewayState
 
 newSocket :: DiscordAuth -> Chan Event -> IO ()
 newSocket (DiscordAuth auth _) events = do
-  void $ forkIO $
-    runSecureClient "gateway.discord.gg" 443 "/?v=6&encoding=json" $ \connection -> do
-      Hello interval <- step connection
+  log <- newChan
+  forkIO (logger log)
+  forkIO $
+    runSecureClient "gateway.discord.gg" 443 "/?v=6&encoding=json" $ \conn -> do
+      Hello interval <- step conn log
       sequenceKey <- newIORef (-1)
-      void $ forkIO $ heartbeat connection interval sequenceKey
-      eventStream connection auth sequenceKey events
-  return ()
+      forkIO $ heartbeat conn interval sequenceKey log
+      eventStream conn auth sequenceKey events log
+      pure ()
+  pure ()
 
-step :: Connection -> IO Payload
-step connection = do
+logger :: Chan String -> IO ()
+logger log = do
+  putStrLn "staring the log"
+  forever $ readChan log >>= putStrLn
+
+step :: Connection -> Chan String -> IO Payload
+step connection log = do
   msg' <- receiveData connection
   case eitherDecode msg' of
     Right msg -> return msg
-    Left  err -> do putStrLn ("Discord-hs.Gateway.Parse" <> err)
-                    putStrLn ("Discord-hs.Gateway.Raw" <> show msg')
+    Left  err -> do writeChan log ("Discord-hs.Gateway.Parse" <> err)
+                    writeChan log ("Discord-hs.Gateway.Raw" <> show msg')
                     return (ParseError err)
 
-heartbeat :: Connection -> Int -> IORef Integer -> IO ()
-heartbeat conn interval sequenceKey = forever $ do
-  num <- readIORef sequenceKey
-  send conn (Heartbeat num)
-  threadDelay (interval * 1000)
+heartbeat :: Connection -> Int -> IORef Integer -> Chan String -> IO ()
+heartbeat conn interval sequenceKey log = do
+  writeChan log "starting heartbeating"
+  forever $ do
+    num <- readIORef sequenceKey
+    writeChan log ("heart " <> show num)
+    send conn (Heartbeat num)
+    writeChan log ("beat " <> show interval)
+    threadDelay (interval * 1000)
 
 setSequence :: IORef Integer -> Integer -> IO ()
 setSequence = writeIORef
@@ -56,20 +68,24 @@ send :: Connection -> Payload -> IO ()
 send conn payload =
   sendTextData conn (encode payload)
 
-eventStream :: Connection -> Auth -> IORef Integer -> Chan Event -> IO ()
-eventStream conn auth sequenceKey eventChan = loop Start
+eventStream :: Connection -> Auth -> IORef Integer -> Chan Event -> Chan String
+    -> IO ()
+eventStream conn auth sequenceKey eventChan log = loop Start
   where
   loop :: GatewayState -> IO ()
   loop Start = do
     send conn (Identify auth False 50 (0, 1))
     loop Running
   loop Running = do
-    payload <- step conn
+    writeChan log "Before step"
+    payload <- step conn log
+    writeChan log "after step"
+    writeChan log $ "Payload - " <> show payload
     case payload of
       Dispatch _ sq _ -> do
         setSequence sequenceKey sq
         case parseDispatch payload of
-          Left reason -> print ("Discord-hs.Gateway.Dispatch - " <> reason)
+          Left reason -> writeChan log ("Discord-hs.Gateway.Dispatch - " <> reason)
           Right event -> do
             writeChan eventChan event
         loop Running
@@ -81,9 +97,8 @@ eventStream conn auth sequenceKey eventChan = loop Start
       InvalidSession -> loop Start
       HeartbeatAck   -> loop Running
       _              -> do
-        putStrLn "Discord-hs.Gateway.Error - InvalidPacket"
-        putStrLn "DYING RIP ME"
+        writeChan log "Discord-hs.Gateway.Error - InvalidPacket"
         loop InvalidDead
-  loop InvalidReconnect = loop InvalidDead
-  loop InvalidDead      = putStrLn "Discord-hs.Gateway.Error - Bot died"
+  loop InvalidReconnect = writeChan log "invalid recon" >> loop InvalidDead
+  loop InvalidDead      = writeChan log "Discord-hs.Gateway.Error - Bot died"
 
