c3dd89c90d20711e8ce8d40aba1a28b3ec8c793d
Karl <37496339+aquarial@users.noreply.github.com>
2018-06-22 22:30:31 -0500

    Write Ready event to events chan (it's important)

--- src/Discord/Gateway/Internal.hs
+++ src/Discord/Gateway/Internal.hs
@@ -55,7 +55,8 @@ connectionLoop auth events log = loop ConnStart
                 send conn (Identify auth False 50 (0, 1)) log
                 msg2 <- step conn log
                 case msg2 of
-                  Right (Dispatch (Ready (Init _ _ _ _ seshID)) _) ->
+                  Right (Dispatch r@(Ready (Init _ _ _ _ seshID)) _) -> do
+                    writeChan events r
                     startEventStream conn events auth seshID interval 0 log
                   _ -> writeChan log ("received2: " <> show msg2) >> pure ConnClosed
               _ -> writeChan log ("received1: " <> show msg) >> pure ConnClosed
