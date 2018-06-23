cfb6f8fa89de415fa16ae5367ca07c888fd21894
Karl <37496339+aquarial@users.noreply.github.com>
2018-06-22 23:01:40 -0500

    Change to globally unique login scheme using UnsafePerformIO

--- src/Discord.hs
+++ src/Discord.hs
@@ -4,38 +4,49 @@ module Discord
   ( module Discord.Rest.Requests
   , module Discord.Types
   , Resp(..)
-  , login
+  , loginRest
+  , loginGateway
   , Discord(..)
-  , Mode(..)
   ) where
 
+import System.IO.Unsafe (unsafePerformIO)
 import Control.Concurrent.Chan
+import Control.Concurrent.MVar
+import Data.Map.Strict as M
 import Data.Aeson
 
 import Discord.Rest
-import Data.HashMap.Strict (empty)
 import Discord.Rest.Requests
 import Discord.Types
 import Discord.Gateway
 
 data Discord = Discord
-  { restCall :: forall a. FromJSON a => Request a -> IO (Resp a)
-  , nextEvent :: IO Event
+  { restCall :: forall a. FromJSON a => Maybe (Request a -> IO (Resp a))
+  , nextEvent :: Maybe (IO Event)
   }
 
-data Mode = Rest | Gateway | RestGateway
+discordLogins :: MVar (M.Map Auth Discord)
+discordLogins = unsafePerformIO (newMVar M.empty)
+{-# NOINLINE discordLogins #-}
 
-login :: Auth -> Mode -> IO Discord
-login auth m = case m of
-  Rest -> do
-    restHandler <- createHandler auth
-    let gate = UnknownEvent "Change mode to RestGateway for this" empty
-    pure $ Discord (writeRestCall restHandler) (pure gate)
-  Gateway -> do
-    chan <- chanWebSocket auth
-    pure $ Discord (\_ -> pure NoResp) (readChan chan)
-  RestGateway -> do
-    chan <- chanWebSocket auth
-    restHandler <- createHandler auth
-    pure $ Discord (writeRestCall restHandler) (readChan chan)
+loginRest :: FromJSON a => Auth -> IO (Request a -> IO (Resp a))
+loginRest auth = do
+  logs <- readMVar discordLogins
+  case M.findWithDefault (Discord Nothing Nothing) auth logs :: Discord of
+    (Discord (Just call) _) -> pure call
+    (Discord  _          e) -> do
+      restHandler <- createHandler auth
+      let nextDisc = Discord (Just (writeRestCall restHandler)) e
+      putMVar discordLogins (M.insert auth nextDisc logs)
+      loginRest auth
 
+loginGateway :: Auth -> IO (IO (Event))
+loginGateway auth = do
+  logs <- readMVar discordLogins
+  case M.findWithDefault (Discord Nothing Nothing) auth logs of
+    (Discord _ (Just e)) -> pure e
+    (Discord r  Nothing) -> do
+      chan <- chanWebSocket auth
+      let nextDisc = Discord r (Just (readChan chan))
+      putMVar discordLogins (M.insert auth nextDisc logs)
+      loginGateway auth
