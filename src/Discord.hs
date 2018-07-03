f764001898b098ed36ce9d9ad7e16a815df12078
Karl <37496339+aquarial@users.noreply.github.com>
2018-07-03 14:27:58 -0500

    Data type holds the chans/mvar, functions do the magic

--- src/Discord.hs
+++ src/Discord.hs
@@ -8,6 +8,9 @@ module Discord
   , Discord(..)
   , Cache(..)
   , login
+  , rest
+  , nextEvent
+  , cache
   ) where
 
 import System.IO.Unsafe (unsafePerformIO)
@@ -25,14 +28,22 @@ import Discord.Gateway.Cache
 newtype RestPart = RestPart { restPart :: forall a. FromJSON a => Request a -> IO (Resp a) }
 
 data Discord = Discord
-  { restCall :: RestPart
-  , nextEvent :: IO Event
-  , info :: IO Cache
+  { _discordRest :: RestChan
+  , _discordEvents :: Chan Event
+  , _discordCache :: MVar Cache
   }
 
 discordLogins :: MVar (M.Map Auth Discord)
 discordLogins = unsafePerformIO (newMVar M.empty)
 {-# NOINLINE discordLogins #-}
+rest :: FromJSON a => Discord -> Request a -> IO (Resp a)
+rest d = writeRestCall (_discordRest d)
+
+nextEvent :: Discord -> IO Event
+nextEvent d = readChan (_discordEvents d)
+
+cache :: Discord -> IO Cache
+cache d = readMVar (_discordCache d)
 
 login :: Auth -> IO Discord
 login auth = do
