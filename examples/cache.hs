2d4f527402b165f538a5bd6b2ed33792a48c04c1
Karl <37496339+aquarial@users.noreply.github.com>
2018-07-08 01:16:20 -0500

    Add example using Cache: prints info once loaded

---
+++ examples/cache.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (forever)
+import Data.Char (isSpace)
+import Data.Monoid ((<>))
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+
+import Discord
+
+-- |Prints every event as it happens
+cacheExample :: IO ()
+cacheExample = do
+  tok <- T.filter (not . isSpace) <$> TIO.readFile "./examples/auth-token.secret"
+  dis <- loginRestGateway (Bot tok)
+  cache <- readCache dis
+
+  putStrLn ("Cached info from gateway: " <> show cache)
+
