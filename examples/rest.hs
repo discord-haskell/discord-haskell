dc6ba1048a55adaae60c46f60192e808508573ab
Karl <37496339+aquarial@users.noreply.github.com>
2018-07-12 22:51:04 -0500

    Use CreateReaction in the rest example

--- examples/rest.hs
+++ examples/rest.hs
@@ -13,12 +13,18 @@ restExample = do
   tok <- T.filter (not . isSpace) <$> TIO.readFile "./examples/auth-token.secret"
   dis <- loginRest (Bot tok)
 
-  msg <- rest dis (CreateMessage 453207241294610444 "A" Nothing)
+  msg <- rest dis (CreateMessage 453207241294610444 "Creating a message" Nothing)
   putStrLn ("Message object: " <> show msg)
 
   putStrLn ""
 
   chan <- rest dis (GetChannel 453207241294610444)
   putStrLn ("Channel object: " <> show chan)
+
+  case msg of
+    Resp m -> do r <- rest dis (CreateReaction (453207241294610444, messageId m) "🐮" Nothing)
+                 putStrLn ("Reaction resp: " <> show r)
+    _ -> putStrLn "Creating the message failed, couldn't react"
+
 
 
