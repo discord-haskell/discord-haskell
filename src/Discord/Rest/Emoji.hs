0cef86065557ab587e866f025d44a3ed24a45748
Karl <37496339+aquarial@users.noreply.github.com>
2018-08-27 19:57:37 -0500

    Supports static & animated guild emoji

--- src/Discord/Rest/Emoji.hs
+++ src/Discord/Rest/Emoji.hs
@@ -36,8 +36,8 @@ data EmojiRequest a where
   ListGuildEmojis :: Snowflake -> EmojiRequest [Emoji]
   -- | Emoji object for the given guild and emoji ID
   GetGuildEmoji :: Snowflake -> Snowflake -> EmojiRequest Emoji
-  -- | Create a new guild emoji. Requires MANAGE_EMOJIS permission.
-  CreateGuildEmojiPng :: Snowflake -> T.Text -> EmojiImage -> EmojiRequest Emoji
+  -- | Create a new guild emoji (static&animated). Requires MANAGE_EMOJIS permission.
+  CreateGuildEmoji :: Snowflake -> T.Text -> EmojiImage -> EmojiRequest Emoji
   -- | Requires MANAGE_EMOJIS permission
   ModifyGuildEmoji :: Snowflake -> Snowflake -> ModifyGuildEmojiOpts -> EmojiRequest Emoji
   -- | Requires MANAGE_EMOJIS permission
@@ -56,22 +56,27 @@ instance ToJSON ModifyGuildEmojiOpts where
 data EmojiImage = EmojiImage [Char]
 
 parseEmojiImage :: Q.ByteString -> Either String EmojiImage
-parseEmojiImage im = do
-      let picE = decodeImage im
-          isPic = case picE of Left _ -> False; Right _ -> True
-          Right pic = picE
-          (w,h) = imageDims pic
-      if | not isPic -> Left "Cannot create emoji - bytestring isn't an image"
-         | w /= 128 || h /= 128 -> Left "Cannot create emoji - Image isn't 128x128"
-         | Q.length im > 256000 -> Left "Cannot create emoji - Image is larger than 256kb"
-         | True -> Right (EmojiImage (Q.unpack (B64.encode im)))
+parseEmojiImage bs =
+  if Q.length bs > 256000
+  then Left "Cannot create emoji - File is larger than 256kb"
+  else case decodeEmoji of
+         Left err -> Left ("Could not parse image or gif.\n\n" <> err)
+         Right im -> if is128 im
+                     then Left "Cannot create emoji - Image isn't 128x128"
+                     else Right (EmojiImage (Q.unpack (B64.encode bs)))
+  where
+    is128 im = let (w,h) = imageDims im
+               in w /= 128 || h /= 128
+    decodeEmoji = case decodeImage bs of
+                    Left _ -> decodeGif bs
+                    Right i -> Right i
 
 
 emojiMajorRoute :: EmojiRequest a -> String
 emojiMajorRoute c = case c of
   (ListGuildEmojis g) ->      "emoji " <> show g
   (GetGuildEmoji g _) ->      "emoji " <> show g
-  (CreateGuildEmojiPng g _ _) -> "emoji " <> show g
+  (CreateGuildEmoji g _ _) -> "emoji " <> show g
   (ModifyGuildEmoji g _ _) -> "emoji " <> show g
   (DeleteGuildEmoji g _)   -> "emoji " <> show g
 
@@ -87,13 +92,13 @@ emojiJsonRequest :: EmojiRequest r -> JsonRequest
 emojiJsonRequest c = case c of
   (ListGuildEmojis g) -> Get (guilds // g) mempty
   (GetGuildEmoji g e) -> Get (guilds // g /: "emojis" // e) mempty
-  (CreateGuildEmojiPng g name (EmojiImage im)) ->
-                      Post (guilds // g /: "emojis")
-                           (pure (R.ReqBodyJson (object [ "name" .= name
-                                                        , "image" .= im
-                                                        -- todo , "roles" .= ...
-                                                        ])))
-                           mempty
+  (CreateGuildEmoji g name (EmojiImage im)) ->
+                   Post (guilds // g /: "emojis")
+                        (pure (R.ReqBodyJson (object [ "name" .= name
+                                                     , "image" .= im
+                                                     -- todo , "roles" .= ...
+                                                     ])))
+                        mempty
   (ModifyGuildEmoji g e o) -> Patch (guilds // g /: "emojis" // e)
                                     (R.ReqBodyJson o)
                                     mempty
