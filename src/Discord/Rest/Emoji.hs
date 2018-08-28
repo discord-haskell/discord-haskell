623aad76dc9d4aedf2cf8ca0e93ca698856f0bcb
Karl <37496339+aquarial@users.noreply.github.com>
2018-08-27 21:54:10 -0500

    Gifs and pngs are parsed separately, but stored in EmojiImage togeth

--- src/Discord/Rest/Emoji.hs
+++ src/Discord/Rest/Emoji.hs
@@ -59,11 +59,19 @@ parseEmojiImage :: Q.ByteString -> Either String EmojiImage
 parseEmojiImage bs =
   if Q.length bs > 256000
   then Left "Cannot create emoji - File is larger than 256kb"
-  else case decodeGifImages bs of
-         Left e -> Left ("Could not parse image or gif.\n\n" <> e)
-         Right ims -> if all is128 gifs
-                      then Right (EmojiImage (Q.unpack (B64.encode bs)))
-                      else Left ("File is not 128x128")
+  else case (decodeGifImages bs, decodeImage bs) of
+         (Left e1, Left e2) -> Left ("Could not parse image or gif: " <> e1
+                                                           <> " and " <> e2)
+         (Right ims, _) -> if all is128 ims
+                           then Right (EmojiImage ("data:text/plain;"
+                                                     <> "base64,"
+                                                     <> Q.unpack (B64.encode bs)))
+                           else Left ("The frames are not all 128x128")
+         (_, Right im) -> if is128 im
+                          then Right (EmojiImage ("data:text/plain;"
+                                                     <> "base64,"
+                                                     <> Q.unpack (B64.encode bs)))
+                          else Left ("Image is not 128x128")
   where
     is128 im = let (w,h) = imageDims im
                in w == 128 || h == 128
