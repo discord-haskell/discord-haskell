e45a29a1adde51e64613dcc1ff78161999efbb35
PixeL <officialpixelinc@gmail.com>
2018-10-15 21:00:00 -0500

    Change parsing

--- src/Discord/Types/Events.hs
+++ src/Discord/Types/Events.hs
@@ -135,7 +135,7 @@ eventParse t o = case t of
     "GUILD_MEMBER_UPDATE"       -> GuildMemberUpdate <$> o .: "guild_id"
                                                      <*> o .: "roles"
                                                      <*> o .: "user"
-                                                     <*> o .: "nick"
+                                                     <*> o .:? "nick"
     "GUILD_MEMBER_CHUNK"        -> GuildMemberChunk <$> o .: "guild_id" <*> o .: "members"
     "GUILD_ROLE_CREATE"         -> GuildRoleCreate  <$> o .: "guild_id" <*> o .: "role"
     "GUILD_ROLE_UPDATE"         -> GuildRoleUpdate  <$> o .: "guild_id" <*> o .: "role"
