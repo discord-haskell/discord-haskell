413d0c144fbfb84fe865487e73b82c4e605ef499
Karl <37496339+aquarial@users.noreply.github.com>
2020-08-04 18:37:04 -0500

    Translate cache example to new

--- examples/cache.hs
+++ examples/cache.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.Text.IO as TIO
+import UnliftIO (liftIO)
 
 import Discord
 
@@ -13,10 +14,10 @@ cacheExample = do
   tok <- TIO.readFile "./examples/auth-token.secret"
 
   _ <- runDiscord $ def { discordToken = tok
-                        , discordOnStart = \dis -> do
-                               cache <- readCache dis
-                               putStrLn ("Cached info from gateway: " <> show cache)
-                               stopDiscord dis
+                        , discordOnStart = do
+                               cache <- readCache
+                               liftIO $ putStrLn ("Cached info from gateway: " <> show cache)
+                               stopDiscord
                         }
   pure ()
 
