fb779fd11c36354bfad56fa48008dbf080410cc3
Karl <37496339+aquarial@users.noreply.github.com>
2021-02-01 15:58:45 -0600

    1.8.2

--- src/Discord/Internal/Rest/Prelude.hs
+++ src/Discord/Internal/Rest/Prelude.hs
@@ -29,7 +29,7 @@ authHeader auth =
   where
   -- | https://discord.com/developers/docs/reference#user-agent
   -- Second place where the library version is noted
-  agent = "DiscordBot (https://github.com/aquarial/discord-haskell, 1.8.1)"
+  agent = "DiscordBot (https://github.com/aquarial/discord-haskell, 1.8.2)"
 
 -- Append to an URL
 infixl 5 //
