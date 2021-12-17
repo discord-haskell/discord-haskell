490478ac219b33698666270e2c5880c78de349a3
L0neGamer <benjaminbwm@gmail.com>
2021-12-17 13:57:49 +0000

    reformatted ping-pong and added info to README.md

--- README.md
+++ README.md
@@ -96,4 +96,124 @@ executable haskell-bot
 - [ ] higher level bot interface? easier to add state and stuff
 - [x] rewrite eventloop [issue](https://github.com/aquarial/discord-haskell/issues/70)
 
+### Installing
+
+discord-haskell is on hosted on hackage at https://hackage.haskell.org/package/discord-haskell, 
+
+In `stack.yaml`
+
+```yaml
+extra-deps:
+- emoji-0.1.0.2
+- discord-haskell-VERSION
+```
+
+In `project.cabal`
+
+```cabal
+executable haskell-bot
+  main-is:             src/Main.hs
+  default-language:    Haskell2010
+  ghc-options:         -threaded
+  build-depends:       base
+                     , text
+                     , discord-haskell
+```
+
+For a more complete example with various options go to 
+[Installing the Library](https://github.com/aquarial/discord-haskell/wiki/Installing-the-Library) wiki page
+
+Also take a look at 
+[Creating your first Bot](https://github.com/aquarial/discord-haskell/wiki/Creating-your-first-Bot)
+for some help setting up your bot token
+
+
+### Emoji
+
+For single character Emoji you can use the unicode name ("eyes", "fire", etc).
+
+For multi-character Emoji you must use the discord format. Type `\:emoji:` into
+a discord chat and paste that into the Text
+
+For example `:thumbsup::skin-tone-3:` is `"👍\127997"`. 
+A custom emoji will look like `<name:id_number>` or `name:id_number`.
+
+See [examples/ping-pong.hs](https://github.com/aquarial/discord-haskell/blob/master/examples/ping-pong.hs)
+ for a `CreateReaction` request in use.
+ 
+### Embeds
+
+Embeds are special messages with boarders and images. [Example embed created by discord-haskell](./examples/embed-photo.jpg)
+
+The `Embed` record (and sub-records) store embed data received from Discord.
+
+The `CreateEmbed` record stores data when we want to create an embed.
+
+`CreateEmbed` has a `Default` instance, so you only need to specify the fields you use:
+
+```haskell
+_ <- restCall (R.CreateMessageEmbed <channel_id> "Pong!" $
+        def { createEmbedTitle = "Pong Embed"
+            , createEmbedImage = Just $ CreateEmbedImageUpload <bytestring>
+            , createEmbedThumbnail = Just $ CreateEmbedImageUrl
+                    "https://avatars2.githubusercontent.com/u/37496339"
+            })
+```
+
+Uploading a file each time is slow, prefer uploading images to a hosting site like imgur.com, and then referencing them.
+ 
+### Limitations
+
+The following features are not implemented:
+
+- Voice & Audio
+- Authenticating with a user token
+
+### Debugging
+
+Always print the userFacingError Text returned from `runDiscord`. I use this to record
+errors that cannot be recovered from.
+
+If something else goes wrong with the library please open an issue. It is helpful,
+but not always necessary, to attach a log of what's going on when the library
+crashes.
+
+Assign a handler to the `discordOnLog :: Text -> IO ()` to print info as it happens.
+Remember to remove sensitive information before posting.
+
+### Application Commands
+
+There are a couple special features discord has added, including slash commands, user commands, and message commands. These can now be created using discord-haskell.
+
+There are three steps to application commands.
+
+1. Register the command with discord. This only needs to be done once, but there is no harm in doing it again/on each reboot, as the creation of commands just overwrites other commands of the same type and name.
+   1. This is achieved by sending a `CreateGuildApplicationCommand` event to discord. The easiest way to do this is by capturing and working off of the `Ready` event in the event loop, which is only sent in the initial startup.
+   2. Commands can either be global (in which case there is a delay to their addition) or they can be guild (server) specific.
+   3. There are other application command requests that can be conducted in this step, so please have a look at the documentation.
+   4. Finally, the application id (which is needed to create commands) of the bot is stored in the cache if you need to create, edit, delete, or get application commands later in the program.
+2. Receive an interaction
+   1. This is achieved by capturing the `Interaction` event in the event loop
+3. Respond to the interaction
+   1. When you have captured an interaction, you can act on the information within, and send a reply using the appropriate method, creating an `InteractionResponse` and sending that with `CreateInteractionResponse`.
+
+Now you're ready to create application commands! If you have any questions, please investigate the source and docs first, and then join the discord above and ask questions there.
+
+### Getting Help
+
+#### Official discord docs
+
+For a list of rest requests, gateway events, and gateway sendables go ggto the 
+[official discord documentation](https://discord.com/developers/docs/intro)
+
+The rest requests line up very closely. The documentation lists 
+[Get Channel](https://discord.com/developers/docs/resources/channel#get-channel)
+and discord-haskell has `GetChannel :: ChannelId -> ChannelRequest Channel`. Same for gateway `Event`s.
+
+#### Examples
+
+The [examples](https://github.com/aquarial/discord-haskell/tree/master/examples) were crafted
+to display a variety of use cases. Read them with care.
+
+#### Open an Issue
 
--- examples/ping-pong.hs
+++ examples/ping-pong.hs
@@ -1,20 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}  -- allows "strings" to be Data.Text
-{-# LANGUAGE  RecordWildCards #-}
+-- allows "strings" to be Data.Text
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
-import Control.Monad (when, forM_, void)
+import Control.Monad (forM_, void, when)
+import Data.Aeson
+import Data.Maybe (fromJust)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-
+import Debug.Trace
+import Discord
+import Discord.Internal.Types.ApplicationCommands
+import qualified Discord.Requests as R
+import Discord.Types
 import UnliftIO (liftIO)
 import UnliftIO.Concurrent
-
-import Discord
-import Discord.Types
-import qualified Discord.Requests as R
-import Debug.Trace
-import Data.Maybe (fromJust)
-import Discord.Internal.Types.ApplicationCommands 
-import Data.Aeson
 
 -- Allows this code to be an executable. See discord-haskell.cabal
 main :: IO ()
@@ -26,16 +25,16 @@ pingpongExample = do
   tok <- TIO.readFile "./examples/auth-token.secret"
 
   -- open ghci and run  [[ :info RunDiscordOpts ]] to see available fields
-  err <- runDiscord $ def { discordToken = tok
-                          , discordOnStart = startHandler
-                          , discordOnEnd = liftIO $ putStrLn "Ended"
-                          , discordOnEvent = eventHandler
-                          , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
-                          }
-
-  -- only reached on an unrecoverable error
-  -- put normal 'cleanup' code in discordOnEnd
-  TIO.putStrLn err
+  t <-
+    runDiscord $
+      def
+        { discordToken = tok,
+          discordOnStart = startHandler,
+          discordOnEnd = liftIO $ putStrLn "Ended",
+          discordOnEvent = eventHandler,
+          discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
+        }
+  TIO.putStrLn t
 
 -- If the start handler throws an exception, discord-haskell will gracefully shutdown
 --     Use place to execute commands you know you want to complete
@@ -43,107 +42,134 @@ startHandler :: DiscordHandler ()
 startHandler = do
   Right partialGuilds <- restCall R.GetCurrentUserGuilds
 
-  let activity = Activity { activityName = "ping-pong"
-                          , activityType = ActivityTypeGame
-                          , activityUrl = Nothing
-                          }
-  let opts = UpdateStatusOpts { updateStatusOptsSince = Nothing
-                              , updateStatusOptsGame = Just activity
-                              , updateStatusOptsNewStatus = UpdateStatusOnline
-                              , updateStatusOptsAFK = False
-                              }
+  let activity =
+        Activity
+          { activityName = "ping-pong",
+            activityType = ActivityTypeGame,
+            activityUrl = Nothing
+          }
+  let opts =
+        UpdateStatusOpts
+          { updateStatusOptsSince = Nothing,
+            updateStatusOptsGame = Just activity,
+            updateStatusOptsNewStatus = UpdateStatusOnline,
+            updateStatusOptsAFK = False
+          }
   sendCommand (UpdateStatus opts)
 
-  -- forM_ partialGuilds $ \pg -> do
-  --   Right guild <- restCall $ R.GetGuild (partialGuildId pg)
-  --   Right chans <- restCall $ R.GetGuildChannels (guildId guild)
-  --   forM_ (take 1 (filter isTextChannel chans))
-  --     (\channel -> restCall $ R.CreateMessage (channelId channel)
-  --                                     "Hello! I will reply to pings with pongs")
+  forM_ partialGuilds $ \pg -> do
+    Right guild <- restCall $ R.GetGuild (partialGuildId pg)
+    Right chans <- restCall $ R.GetGuildChannels (guildId guild)
+    forM_
+      (take 1 (filter isTextChannel chans))
+      ( \channel ->
+          restCall $
+            R.CreateMessage
+              (channelId channel)
+              "Hello! I will reply to pings with pongs"
+      )
 
+exampleSlashCommand :: CreateApplicationCommand
+exampleSlashCommand =
+  CreateApplicationCommand
+    "test"
+    "here is a description"
+    ( Just
+        [ ApplicationCommandOption
+            STRING
+            "randominput"
+            "I shall not"
+            (Just True)
+            ( Just
+                [ ApplicationCommandOptionChoice "firstopt" (SNS "yay"),
+                  ApplicationCommandOptionChoice "secondopt" (SNS "nay")
+                ]
+            )
+            Nothing
+            Nothing
+            Nothing
+            Nothing
+            Nothing
+        ]
+    )
+    Nothing
+    Nothing
+
+exampleInteractionResponse :: Maybe [ApplicationCommandInteractionDataOption] -> InteractionResponse
+exampleInteractionResponse d =
+  InteractionResponse
+    CHANNEL_MESSAGE_WITH_SOURCE
+    ( Just
+        ( ICDM
+            ( InteractionCallbackMessages
+                Nothing
+                ( Just
+                    ( T.pack $
+                        "Here's the reply! You chose: "
+                          ++ show (fromJust $ applicationCommandInteractionDataOptionValue $ head (fromJust d))
+                    )
+                )
+                Nothing
+                Nothing
+                Nothing
+                Nothing
+            )
+        )
+    )
 
 -- If an event handler throws an exception, discord-haskell will continue to run
 eventHandler :: Event -> DiscordHandler ()
 eventHandler event = case event of
-      MessageCreate m -> when (not (fromBot m) && isPing m) $ do
-        void $ restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
-        threadDelay (2 * 10^(6 :: Int))
+  MessageCreate m -> when (not (fromBot m) && isPing m) $ do
+    void $ restCall (R.CreateReaction (messageChannel m, messageId m) "eyes")
+    threadDelay (2 * 10 ^ (6 :: Int))
 
-        -- A very simple message.
-        void $ restCall (R.CreateMessage (messageChannel m) "Pong!")
+    -- A very simple message.
+    void $ restCall (R.CreateMessage (messageChannel m) "Pong!")
 
-        -- A more complex message. Text-to-speech, does not mention everyone nor
-        -- the user, and uses Discord native replies.
-        -- Use ":info" in ghci to explore the type
-        let opts :: R.MessageDetailedOpts
-            opts = def { R.messageDetailedContent = "Here's a more complex message, but doesn't ping @everyone!"
-                       , R.messageDetailedTTS = True
-                       , R.messageDetailedAllowedMentions = Just $
-                          def { R.mentionEveryone = False
-                              , R.mentionRepliedUser = False
-                              }
-                       , R.messageDetailedReference = Just $
-                          def { referenceMessageId = Just $ messageId m }
-                       }
-        void $ restCall (R.CreateMessageDetailed (messageChannel m) opts)
-      Ready _ _ _ _ _ _ pa@(PartialApplication i f) -> 
-        trace (show pa) (
-          restCall (
-            R.CreateGuildApplicationCommand i serverid (
-              CreateApplicationCommand 
-                "test" 
-                "here is a description" 
-                (Just 
-                  [
-                    ApplicationCommandOption 
-                      STRING 
-                      "randominput" 
-                      "I shall not" 
-                      (Just True) 
-                      (Just 
-                        [
-                          ApplicationCommandOptionChoice "firstopt" (SNS "yay"), 
-                          ApplicationCommandOptionChoice "secondopt" (SNS "nay")
-                        ]
-                      ) Nothing Nothing Nothing Nothing Nothing 
-                  ]
-                ) 
-                Nothing
-                Nothing
-            ) 
+    -- A more complex message. Text-to-speech, does not mention everyone nor
+    -- the user, and uses Discord native replies.
+    -- Use ":info" in ghci to explore the type
+    let opts :: R.MessageDetailedOpts
+        opts =
+          def
+            { R.messageDetailedContent = "Here's a more complex message, but doesn't ping @everyone!",
+              R.messageDetailedTTS = True,
+              R.messageDetailedAllowedMentions =
+                Just $
+                  def
+                    { R.mentionEveryone = False,
+                      R.mentionRepliedUser = False
+                    },
+              R.messageDetailedReference =
+                Just $
+                  def {referenceMessageId = Just $ messageId m}
+            }
+    void $ restCall (R.CreateMessageDetailed (messageChannel m) opts)
+  Ready _ _ _ _ _ _ pa@(PartialApplication i f) ->
+    trace
+      (show pa)
+      ( restCall
+          ( R.CreateGuildApplicationCommand i serverid exampleSlashCommand
           )
-        ) >>= \rs -> trace (show rs) (return ())
-      InteractionCreate i@Interaction {..} ->  trace (show i) $ do
-        let cid = fromJust interactionChannelId
-        if interactionType /= APPLICATION_COMMAND then void $ restCall (R.CreateMessage cid "I don't know how to handle an interaction like that!")
-        else do
-          let d = fromJust interactionData
-          case interactionDataApplicationCommandType d of
-            ACTCHAT_INPUT -> void $  restCall (
-                let x = InteractionResponse 
-                            CHANNEL_MESSAGE_WITH_SOURCE 
-                            (Just 
-                              (ICDM 
-                                (InteractionCallbackMessages 
-                                  Nothing 
-                                  (Just 
-                                    (T.pack $ "Here's the reply! You chose: " 
-                                      ++ show (fromJust $ applicationCommandInteractionDataOptionValue $ head  (fromJust $ interactionDataOptions d))
-                                    )
-                                  )
-                                  Nothing
-                                  Nothing
-                                  Nothing
-                                  Nothing
-                                )
-                              )
-                            )
-                          in trace (show (toJSON x)) (R.CreateInteractionResponse interactionId interactionToken x)
+      )
+      >>= \rs -> trace (show rs) (return ())
+  InteractionCreate i@Interaction {..} -> trace (show i) $ do
+    let cid = fromJust interactionChannelId
+    if interactionType /= APPLICATION_COMMAND
+      then void $ restCall (R.CreateMessage cid "I don't know how to handle an interaction like that!")
+      else do
+        let d = fromJust interactionData
+        case interactionDataApplicationCommandType d of
+          ACTCHAT_INPUT ->
+            void $
+              restCall
+                ( R.CreateInteractionResponse interactionId interactionToken (exampleInteractionResponse (interactionDataOptions d))
                 )
-            _ -> void $ restCall (R.CreateMessage cid "I got some other kind of application command!")
-      -- Note that the above is not the required way of receiving and replying to interactions - this is marked as a failure
+          _ -> void $ restCall (R.CreateMessage cid "I got some other kind of application command!")
+  -- Note that the above is not the required way of receiving and replying to interactions - this is marked as a failure
 
-      _ -> return ()
+  _ -> return ()
   where
     serverid = -1
 
