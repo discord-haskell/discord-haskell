6597a06d646529974f0e76844e5b01a16fca0c55
Karl <37496339+aquarial@users.noreply.github.com>
2022-01-08 16:12:58 -0600

    Revert "Merge pull request #82 from L0neGamer/emoji-animated"
    This reverts commit 086c0e154f4dce68d810009854160d7ceefab112, reversing
    changes made to 4130af2e6c395a8e6f3baf0ff26c964b66e78b3b.

--- src/Discord/Internal/Types/Channel.hs
+++ src/Discord/Internal/Types/Channel.hs
@@ -260,12 +260,13 @@ instance FromJSON MessageReaction where
 
 -- | Represents an emoticon (emoji)
 data Emoji = Emoji
-  { emojiId       :: Maybe EmojiId  -- ^ The emoji id
-  , emojiName     :: Maybe T.Text   -- ^ The emoji name
-  , emojiRoles    :: Maybe [RoleId] -- ^ Roles the emoji is active for
-  , emojiUser     :: Maybe User     -- ^ User that created this emoji
-  , emojiManaged  :: Maybe Bool     -- ^ Whether this emoji is managed
+  { emojiId      :: Maybe EmojiId  -- ^ The emoji id
+  , emojiName    :: Maybe T.Text   -- ^ The emoji name
+  , emojiRoles   :: Maybe [RoleId] -- ^ Roles the emoji is active for
+  , emojiUser    :: Maybe User     -- ^ User that created this emoji
+  , emojiManaged :: Maybe Bool     -- ^ Whether this emoji is managed
   , emojiAnimated :: Maybe Bool     -- ^ Whether this emoji is animated
+  -- TODO MAY NEED TO REMOVE EMOJI ANIMATED
   } deriving (Show, Read, Eq, Ord)
 
 instance FromJSON Emoji where
@@ -275,7 +276,6 @@ instance FromJSON Emoji where
           <*> o .:? "roles"
           <*> o .:? "user"
           <*> o .:? "managed"
-          <*> o .:? "animated"
 
 
 -- | Represents an attached to a message file.
