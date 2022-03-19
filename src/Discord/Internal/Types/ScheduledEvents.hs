a12bc7c29776de67a82242a4834c4d0c4c482f41
Antoine "Annwan" Combet <annwan@annwan.me>
2022-03-19 15:21:31 +0100

    instance FromJSON ScheduledEvent
    Also fixed the ToJSON instances to match the style of the rest of the library

--- src/Discord/Internal/Types/ScheduledEvents.hs
+++ src/Discord/Internal/Types/ScheduledEvents.hs
@@ -1,29 +1,42 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 -- | Structures pertaining to Discord Scheduled Events
 module Discord.Internal.Types.ScheduledEvents where
 
-import           Data.Aeson                     ( (.=)
+import           Data.Aeson                     ( (.:)
+                                                , (.:?)
+                                                , (.=)
+                                                , FromJSON(parseJSON)
                                                 , ToJSON(toJSON)
-                                                , Value(Null, Number, Object)
+                                                , Value(Number)
                                                 , object
+                                                , withObject
                                                 )
-import           Data.HashMap.Strict            ( insert )
-import           Data.Maybe                     ( isJust )
+import           Data.Aeson.Types               ( Parser )
+import           Data.Data                      ( Data )
 import qualified Data.Text                     as T
 import           Data.Time                      ( UTCTime )
 import           Discord.Internal.Types.Prelude ( ChannelId
                                                 , GuildId
+                                                , InternalDiscordEnum
+                                                  ( discordTypeParseJSON
+                                                  , discordTypeStartValue
+                                                  , fromDiscordType
+                                                  )
                                                 , Snowflake
                                                 , UserId
+                                                , toMaybeJSON
                                                 )
 import           Discord.Internal.Types.User    ( User )
+
 
 type ScheduledEventId = Snowflake
 
 type ScheduledEventEntityId = Snowflake
 
+-- | The ScheduledEvent data structure
 data ScheduledEvent
   = ScheduledEventStage
       { scheduledEventStageId :: ScheduledEventId
@@ -78,111 +91,172 @@ data ScheduledEvent
       }
 
 instance ToJSON ScheduledEvent where
-  toJSON ScheduledEventExternal {..} =
-    Object
-      $ (if isJust scheduledEventExternalUserCount
-          then insert "user_count" (toJSON scheduledEventExternalUserCount)
-          else id
-        )
-      $ (if isJust scheduledEventExternalCreator
-          then insert "creator" (toJSON scheduledEventExternalCreator)
-          else id
-        )
-          base
-   where
-    Object base = object
-      [ "id" .= scheduledEventExternalId
-      , "guild_id" .= scheduledEventExternalGuildId
-      , "channel_id" .= Null
-      , "name" .= scheduledEventExternalName
-      , "creator_id" .= scheduledEventExternalCreatorId
-      , "description" .= scheduledEventExternalDescription
-      , "scheduled_start_time" .= scheduledEventExternalStartTime
-      , "scheduled_end_time" .= scheduledEventExternalEndTime
-      , "privacy_level" .= scheduledEventExternalPrivacyLevel
-      , "status" .= scheduledEventExternalStatus
-      , "entity_type" .= Number 3
-      , "entity_id" .= scheduledEventExternalEntityId
-      , "entity_metadata"
-        .= object ["location" .= scheduledEventExternalLocation]
-      , "creator" .= scheduledEventExternalCreator
-      , "user_count" .= scheduledEventExternalUserCount
-      , "image" .= scheduledEventExternalImage
+  toJSON ScheduledEventStage {..} = object
+    [ (name, value)
+    | (name, Just value) <-
+      [ ("id"                  , toMaybeJSON scheduledEventStageId)
+      , ("guild_id"            , toMaybeJSON scheduledEventStageGuildId)
+      , ("channel_id"          , toMaybeJSON scheduledEventStageChannelId)
+      , ("creator_id"          , toJSON <$> scheduledEventStageCreatorId)
+      , ("name"                , toMaybeJSON scheduledEventStageName)
+      , ("description"         , toJSON <$> scheduledEventStageDescription)
+      , ("scheduled_start_time", toMaybeJSON scheduledEventStageStartTime)
+      , ("scheduled_end_time"  , toJSON <$> scheduledEventStageEndTime)
+      , ("privacy_level"       , toMaybeJSON scheduledEventStagePrivacyLevel)
+      , ("entity_type"         , Just $ Number 1)
+      , ("entity_id"           , toJSON <$> scheduledEventStageEntityId)
+      , ("creator"             , toJSON <$> scheduledEventStageCreator)
+      , ("user_count"          , toJSON <$> scheduledEventStageUserCount)
+      , ("image"               , toJSON <$> scheduledEventStageImage)
       ]
-  toJSON ScheduledEventStage {..} =
-    Object
-      $ (if isJust scheduledEventStageUserCount
-          then insert "user_count" (toJSON scheduledEventStageUserCount)
-          else id
-        )
-      $ (if isJust scheduledEventStageCreator
-          then insert "creator" (toJSON scheduledEventStageCreator)
-          else id
-        )
-          base
-   where
-    Object base = object
-      [ "id" .= scheduledEventStageId
-      , "guildId" .= scheduledEventStageGuildId
-      , "channel_id" .= scheduledEventStageChannelId
-      , "creator_id" .= scheduledEventStageCreatorId
-      , "name" .= scheduledEventStageName
-      , "description" .= scheduledEventStageDescription
-      , "scheduled_start_time" .= scheduledEventStageStartTime
-      , "scheduled_end_time" .= scheduledEventStageEndTime
-      , "privacy_level" .= scheduledEventStagePrivacyLevel
-      , "status" .= scheduledEventStageStatus
-      , "entity_type" .= Number 1
-      , "entity_id" .= scheduledEventStageEntityId
-      , "entity_metadata" .= Null
-      , "creator" .= scheduledEventStageCreator
-      , "user_count" .= scheduledEventStageUserCount
-      , "image" .= scheduledEventStageImage
+    ]
+  toJSON ScheduledEventVoice {..} = object
+    [ (name, value)
+    | (name, Just value) <-
+      [ ("id"                  , toMaybeJSON scheduledEventVoiceId)
+      , ("guild_id"            , toMaybeJSON scheduledEventVoiceGuildId)
+      , ("channel_id"          , toMaybeJSON scheduledEventVoiceChannelId)
+      , ("creator_id"          , toJSON <$> scheduledEventVoiceCreatorId)
+      , ("name"                , toMaybeJSON scheduledEventVoiceName)
+      , ("description"         , toJSON <$> scheduledEventVoiceDescription)
+      , ("scheduled_start_time", toMaybeJSON scheduledEventVoiceStartTime)
+      , ("scheduled_end_time"  , toJSON <$> scheduledEventVoiceEndTime)
+      , ("privacy_level"       , toMaybeJSON scheduledEventVoicePrivacyLevel)
+      , ("entity_type"         , Just $ Number 2)
+      , ("entity_id"           , toJSON <$> scheduledEventVoiceEntityId)
+      , ("creator"             , toJSON <$> scheduledEventVoiceCreator)
+      , ("user_count"          , toJSON <$> scheduledEventVoiceUserCount)
+      , ("image"               , toJSON <$> scheduledEventVoiceImage)
       ]
-  toJSON ScheduledEventVoice {..} =
-    Object
-      $ (if isJust scheduledEventVoiceUserCount
-          then insert "user_count" (toJSON scheduledEventVoiceUserCount)
-          else id
+    ]
+  toJSON ScheduledEventExternal {..} = object
+    [ (name, value)
+    | (name, Just value) <-
+      [ ("id"                  , toMaybeJSON scheduledEventExternalId)
+      , ("guild_id"            , toMaybeJSON scheduledEventExternalGuildId)
+      , ("creator_id"          , toJSON <$> scheduledEventExternalCreatorId)
+      , ("name"                , toMaybeJSON scheduledEventExternalName)
+      , ("description"         , toJSON <$> scheduledEventExternalDescription)
+      , ("scheduled_start_time", toMaybeJSON scheduledEventExternalStartTime)
+      , ("scheduled_end_time"  , toMaybeJSON scheduledEventExternalEndTime)
+      , ("privacy_level", toMaybeJSON scheduledEventExternalPrivacyLevel)
+      , ("entity_type"         , Just $ Number 3)
+      , ("entity_id"           , toJSON <$> scheduledEventExternalEntityId)
+      , ("creator"             , toJSON <$> scheduledEventExternalCreator)
+      , ("user_count"          , toJSON <$> scheduledEventExternalUserCount)
+      , ("image"               , toJSON <$> scheduledEventExternalImage)
+      , ( "entity_metadata"
+        , Just $ object ["location" .= toJSON scheduledEventExternalLocation]
         )
-      $ (if isJust scheduledEventVoiceCreator
-          then insert "creator" (toJSON scheduledEventVoiceCreator)
-          else id
-        )
-          base
-   where
-    Object base = object
-      [ "id" .= scheduledEventVoiceId
-      , "guildId" .= scheduledEventVoiceGuildId
-      , "channel_id" .= scheduledEventVoiceChannelId
-      , "creator_id" .= scheduledEventVoiceCreatorId
-      , "name" .= scheduledEventVoiceName
-      , "description" .= scheduledEventVoiceDescription
-      , "scheduled_start_time" .= scheduledEventVoiceStartTime
-      , "scheduled_end_time" .= scheduledEventVoiceEndTime
-      , "privacy_level" .= scheduledEventVoicePrivacyLevel
-      , "status" .= scheduledEventVoiceStatus
-      , "entity_type" .= Number 2
-      , "entity_id" .= scheduledEventVoiceEntityId
-      , "entity_metadata" .= Null
-      , "creator" .= scheduledEventVoiceCreator
-      , "user_count" .= scheduledEventVoiceUserCount
-      , "image" .= scheduledEventVoiceImage
       ]
+    ]
 
+
+instance FromJSON ScheduledEvent where
+  parseJSON = withObject
+    "ScheduledEvent"
+    (\v -> do
+      setype <- v .: "entity_type" :: Parser Int
+      seid   <- v .: "id"
+      segid  <- v .: "guild_id"
+      secrid <- v .:? "creator_id"
+      sename <- v .: "name"
+      sedesc <- v .:? "description"
+      sest   <- v .: "scheduled_start_time"
+      sepl   <- v .: "privacy_level" :: Parser ScheduledEventPrivacyLevel
+      sestat <- v .: "status" :: Parser ScheduledEventStatus
+      seeid  <- v .:? "entity_id"
+      secrea <- v .:? "creator"
+      seuc   <- v .:? "user_count"
+      seim   <- v .:? "image"
+
+      case setype of
+        1 -> do
+          sechid <- v .: "channelId"
+          seet   <- v .:? "scheduled_end_time"
+          return $ ScheduledEventStage seid
+                                       segid
+                                       sechid
+                                       secrid
+                                       sename
+                                       sedesc
+                                       sest
+                                       seet
+                                       sepl
+                                       sestat
+                                       seeid
+                                       secrea
+                                       seuc
+                                       seim
+        2 -> do
+          sechid <- v .: "channelId"
+          seet   <- v .:? "scheduled_end_time"
+          return $ ScheduledEventVoice seid
+                                       segid
+                                       sechid
+                                       secrid
+                                       sename
+                                       sedesc
+                                       sest
+                                       seet
+                                       sepl
+                                       sestat
+                                       seeid
+                                       secrea
+                                       seuc
+                                       seim
+        3 -> do
+          semeta <- v .: "entity_metadata"
+          seloc  <- withObject "entity_metadata" (.: "location") semeta
+          seet   <- v .: "scheduled_end_time"
+          return $ ScheduledEventExternal seid
+                                          segid
+                                          seloc
+                                          secrid
+                                          sename
+                                          sedesc
+                                          sest
+                                          seet
+                                          sepl
+                                          sestat
+                                          seeid
+                                          secrea
+                                          seuc
+                                          seim
+        _ -> error "unreachable"
+    )
+
+-- | The privacy level of a scheduled event
 data ScheduledEventPrivacyLevel = ScheduledEventPrivacyLevelGuildOnly
+  deriving (Show, Read, Eq, Ord, Data)
+
+instance InternalDiscordEnum ScheduledEventPrivacyLevel where
+  discordTypeStartValue = ScheduledEventPrivacyLevelGuildOnly
+  fromDiscordType ScheduledEventPrivacyLevelGuildOnly = 2
 
 instance ToJSON ScheduledEventPrivacyLevel where
-  toJSON ScheduledEventPrivacyLevelGuildOnly = Number 2
+  toJSON = toJSON . fromDiscordType
 
+instance FromJSON ScheduledEventPrivacyLevel where
+  parseJSON = discordTypeParseJSON "ScheduledEventPrivacyLevel"
+
+-- | The Status of a Scheduled Event
 data ScheduledEventStatus
   = ScheduledEventStatusScheduled
   | ScheduledEventStatusActive
   | ScheduledEventStatusCompleted
   | ScheduledEventStatusCancelled
+  deriving (Show, Read, Eq, Ord, Data)
+
+instance InternalDiscordEnum ScheduledEventStatus where
+  discordTypeStartValue = ScheduledEventStatusScheduled
+  fromDiscordType ScheduledEventStatusScheduled = 1
+  fromDiscordType ScheduledEventStatusActive    = 2
+  fromDiscordType ScheduledEventStatusCompleted = 3
+  fromDiscordType ScheduledEventStatusCancelled = 4
 
 instance ToJSON ScheduledEventStatus where
-  toJSON ScheduledEventStatusScheduled = Number 1
-  toJSON ScheduledEventStatusActive    = Number 2
-  toJSON ScheduledEventStatusCompleted = Number 3
-  toJSON ScheduledEventStatusCancelled = Number 4
+  toJSON = toJSON . fromDiscordType
+
+instance FromJSON ScheduledEventStatus where
+  parseJSON = discordTypeParseJSON "ScheduledEventStatus"
