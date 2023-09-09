bedbf95eb190b960450fbcf1eb4b9d4e47bd6bca
L0neGamer <benjaminbwm@gmail.com>
2023-09-09 19:09:04 +0100

    fix cache example

--- examples/cache.hs
+++ examples/cache.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 import UnliftIO (liftIO)
 
 import Discord
+import Discord.Types
+
+import qualified Data.Text as T
 
 import ExampleUtils (getToken)
 
@@ -12,16 +16,20 @@ main = cacheExample
 -- There's not much information in the Cache for now
 --   but this program will show you what its got
 
+-- Note that the cache has to be enabled, and that this program will only print 
+-- out when a message is received.
+
 -- | Print cached Gateway info
 cacheExample :: IO ()
 cacheExample = do
   tok <- getToken
 
-  _ <- runDiscord $ def { discordToken = tok
-                        , discordOnStart = do
-                               cache <- readCache
-                               liftIO $ putStrLn ("Cached info from gateway: " <> show cache)
-                               stopDiscord
+  t <- runDiscord $ def { discordToken = tok
+                        , discordOnLog = putStrLn . T.unpack
+                        , discordOnEvent = \case
+                            MessageCreate _ -> readCache >>= liftIO . putStrLn . ("Current info as of new message: " <>) . show
+                            _ -> pure ()
+                        , discordEnableCache = True
                         }
-  pure ()
+  print t
 
