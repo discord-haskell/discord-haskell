170d235dc243a40f10c16f52c84210bc786b3c23
Karl <37496339+aquarial@users.noreply.github.com>
2023-11-27 11:58:02 -0600

    start adding fullapplication

--- src/Discord/Internal/Types/ApplicationInfo.hs
+++ src/Discord/Internal/Types/ApplicationInfo.hs
@@ -12,8 +12,17 @@ import Discord.Internal.Types.Prelude
 data FullApplication = FullApplication
   { fullApplicationID :: ApplicationId
   , fullApplicationName :: T.Text
-  , fullApplicationFlags :: Int
-  } deriving (Show, Eq, Read)
+  , fullApplicationIcon :: Maybe T.Text -- ^ Icon hash
+  , fullApplicationDescription :: Maybe T.Text
+  , fullApplicationRPCOrigins :: [T.Text] -- ^ List of RPC origin URLs, if RPC is enabled
+  , fullApplicationBotPublic :: Bool -- ^ When false, only the app owner can add the app to guilds
+  , fullApplicationRequiresCodeGrant Bool -- ^ When true, the app's bot will only join upon completion of the full OAuth2 code grant flow
+  -- , fullApplicationBot :: User -- partial user, test what it has?
+  , fullApplicationTermsOfServiceUrl :: T.Text
+  , fullApplicationPrivacyPolicyUrl :: T.Text
+  -- , fullApplicationOwner :: User -- ^ partial user, test what it has?
+  , fullApplicationVerifyKey :: T.Text -- ^ Hex encoded key for verification in interactions and the GameSDK's GetTicket
+  -- , fullApplicationTeam :: Team -- ^ 
 
 instance FromJSON FullApplication where
   parseJSON = withObject "FullApplication" $ \o ->
